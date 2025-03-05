8af4dc7eb4283ad54d926d3264510b30452d80b2
gominibui <164637350+gominibui@users.noreply.github.com>
2025-03-05 10:59:40 +0200

    add CRUD Event manager

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    organizer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
